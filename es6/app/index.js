0bafb4a19c48cdca61dc4d22e18f1eaf48bfda57
Pablo Gonzalez <pablogr.gonzalez@gmail.com>
2018-02-12 23:42:23 -0600

    React weather app
    Signed-off-by: Pablo Gonzalez <pablogr.gonzalez@gmail.com>

--- es6/app/index.js
+++ es6/app/index.js
@@ -1,10 +1,34 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 
+const api_key = '6415d962ffe762edc096a4bfd0c8d168';
+
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            city: 'Aguascalientes'
+        }
+    }
+
+    componentDidMount() {
+        this.grabWeather(this.state.city)
+    }
+
+    grabWeather(city) {
+        fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`)
+            .then(response => response.json())
+            .then(obj => {
+                this.setState({description: obj.weather[0].description})
+            });
+    }
+
     render() {
         return (
-            <div>React JS and JSX in action!</div>
+            <div>
+                <h3>Weather Report for {this.state.city}</h3>
+                <p>{this.state.description}</p>
+            </div>
         )
     }
 }
