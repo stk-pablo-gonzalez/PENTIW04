991c98f75cc0d81f2d0fab3e32868ea3edc34d75
Pablo Gonzalez <pablo.gonzalez@softtek.com>
2018-02-14 00:01:41 -0600

    Complete weather report

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # PENTIW04
 Essentials in JavaScript ES6
+
+## Overview
+This project contains the following demo:
+    * **Weather Report:** Provides current weather information using ES6, React and OpenWeatherMap.org data.
+
+## Requirements
+This project requires the following to be installed:
+    * NodeJS
+
+## Installation Instructions
+First install dependencies:
+```sh
+$ npm install
+```
+
+Then use npm to build and start the WebPack server:
+```sh
+$ npm start
+````
+
+Open a browser to open *http://localhost:3000/*
--- app/index.js
+++ app/index.js
@@ -1,10 +1,58 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import Weather from './weather';
+
+const api_key = '6415d962ffe762edc096a4bfd0c8d168';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            city: {
+                name: 'Aguascalientes',
+                coord: {
+                    lon: 0,
+                    lat: 0
+                }
+            },
+            conditions: {
+                description: '',
+                temperature: 0,
+                humidity: 0
+            }
+        }
+        this.weatherService = new Weather();
+    }
+
+    componentDidMount() {
+        this.weatherService.grab(this.state.city.name)
+            .then(w => {
+                this.setState({
+                    city: {
+                        name: w.name,
+                        coord: {
+                            lon: w.coord.lon,
+                            lat: w.coord.lat
+                        }
+                    },
+                    conditions: {
+                        description: w.weather[0].main,
+                        temperature: w.main.temp,
+                        humidity: w.main.humidity
+                    }
+                });
+                console.log(w);
+            });
+    }
+
     render() {
         return (
-            <div>React JS and JSX in action</div>
+            <div>
+                <h1>Weather Report for {this.state.city.name}</h1>
+                <h2>Current Conditions</h2>
+                <h3>{this.state.conditions.temperature}°C {this.state.conditions.description}</h3>
+                
+            </div>
         )
     }
 }
